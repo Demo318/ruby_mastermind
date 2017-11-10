9d0918fb9096af65670ecbc9cdf939bb59ff9e37
Bobby Ratliff <ratliff.bobby@gmail.com>
2017-11-09 21:31:03 -0600

    Make ai smarter.
    Looks at the first turn to eliminate wrong digits.
    Looks at the most recent turn for right spots.

--- game_resources/ai.rb
+++ game_resources/ai.rb
@@ -13,10 +13,32 @@ module AI
   end
 
   def guess_code(prev_guesses_hash, prev_feedback_hash, ai_brain) #returns array of four digits
+    candidate_digits = (1..6).to_a
+    if prev_feedback_hash.length > 0
+      # why quoted?
+      prev_feedback_hash["0"].each_with_index do |item, idx|
+        if item == "X"
+          candidate_digits = candidate_digits.reject { |i| i == prev_guesses_hash["0"][idx] }
+        end
+      end
+    end
+    puts candidate_digits
     guess_code = Array.new
     while guess_code.length < 4
-      num = rand(1..6)
-      guess_code << num if guess_code.include?(num) == false
+      if prev_feedback_hash.length > 0
+        last_turn = (prev_feedback_hash.length-1).to_s
+        prev_feedback_hash[last_turn].each_with_index do |item, idx|
+          if item == "!"
+            guess_code << prev_guesses_hash[last_turn][idx]
+          else
+            guess_code << candidate_digits.sample
+          end
+        end
+  
+      else
+        num = candidate_digits.sample
+        guess_code << num if guess_code.include?(num) == false
+      end
     end
     sleep(1)
     guess_code
